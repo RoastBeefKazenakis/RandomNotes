14960e7819cb9c58923ae1abfa9c3afb2464a803
Thomas Sabino-Benowitz <53605757+RoastBeefKazenakis@users.noreply.github.com>
2020-10-15 23:46:20 -0400

    add Swift Playgrounds

---
+++ Architecture/swift playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// Complete the repeatedString function below.
+func repeatedString(s: String, n: Int) -> Int {
+    let letters = s.components
+    for
+    
+}
