599b22bdfd36c5b03f4c23deba04b0ebb20383c7
Thomas Sabino-Benowitz <53605757+RoastBeefKazenakis@users.noreply.github.com>
2020-10-21 23:57:17 -0400

    add Leetcode problem I'm working on

---
+++ leetcode.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+'''
+Given an integer, s , find and print the number of letter a's 
+in the first n letters of Lilah's infinite string.
+
+For example, if the string s = "abcabc" and n = 10 , the substring we consider is abcacabcac, 
+the first 10 characters of her infinite string. There are 4 occurrences of a in the substring.
+'''
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
